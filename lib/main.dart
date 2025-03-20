c5c32c3cb07266b17d2deb4b15c45402c031c409
DIEGO_JESUS_RODRIGUEZ_CHAVEZ <a.22308051281296@cbtis128.edu.mx>
2025-03-20 18:39:44 +0000

    envia

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Stack con 3 Propiedades",
+            style: TextStyle(color: Color(0xff000000)),
+          ),
+          backgroundColor: Color(0xff8adeff), // Color azul claro para el AppBar
+          elevation: 10, // Sombra en el AppBar
+          automaticallyImplyLeading: false,
+        ),
+        body: Center(
+          child: Stack(
+            // Propiedad 1: Alineación de los hijos dentro del Stack
+            alignment: Alignment.center, // Alinea los hijos en el centro
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+            // Propiedad 2: Comportamiento de recorte (clip)
+            clipBehavior:
+                Clip.none, // Permite que los hijos se desborden del Stack
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+            // Propiedad 3: Lista de hijos (widgets) dentro del Stack
+            children: <Widget>[
+              // Fondo con un Container
+              Container(
+                width: 200,
+                height: 200,
+                color: Color(0xff3975a8),
+              ),
+
+              // Texto en la parte superior
+              Positioned(
+                top: 10,
+                child: Text(
+                  'Texto arriba',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+
+              // Texto en el centro
+              Text(
+                'Texto centro',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              // Texto en la parte inferior
+              Positioned(
+                bottom: 10,
+                child: Text(
+                  'Texto abajo',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
